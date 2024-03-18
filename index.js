364b87aaa584b13333e9142b01862c787b951690
Raghav Garg <raghav1107.be21@chitkara.edu.in>
2024-03-18 13:18:29 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.get('/' , (req , res) => {
     res.json("Hello World");
