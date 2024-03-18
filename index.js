bc9c8dd15de7739970f4b68ca0aadc35b0994010
Raghav Garg <raghav1107.be21@chitkara.edu.in>
2024-03-18 13:03:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/bhfl' , (req , res) => {
+    const {data} = req.body;
+    var odd_numbers = [];
+    var even_numbers = [];
+    var alphabets = [];
+
+    data.forEach(str => {
+        if(str.length === 1 && str.match(/[a-z]/i)){
+            alphabets.push(str.toUpperCase());
+        }else{
+            var num = parseInt(str);
+            if(str % 2 == 0){
+                even_numbers.push(num);
+            }else{
+                odd_numbers.push(num);
+            }
+        }
+    });
+
+    return res.json({
+        "is_success" : true ,
+        "user_id" : "garg_raghav_18092004" ,
+        "roll_number" : "2110991107" ,
+        odd_numbers ,
+        even_numbers ,
+        alphabets
+    });
+})
+
+app.listen(3000 , (PORT) => {
+    console.log("Server Lisetning on " , PORT);
+});
